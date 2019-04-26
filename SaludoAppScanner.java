88c30e0c526ec1b876e244291bbefcb0b95dff13
L-Guzman <49572675+L-Guzman@users.noreply.github.com>
2019-04-26 05:16:36 -0600

    Add files via upload

---
+++ SaludoAppScanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SaludoAppScanner {
+	public static void main(String args[]) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Introduce tu nombre");
+		String nombre=sc.nextLine();
+		
+		System.out.println("Bienvenido " +nombre);
+	}
+	
+
+}
---
+++ SumaNum.java
@@ -0,0 +1,13 @@
+
+public class SumaNum {
+	public static void main (String args[]) {
+		
+		
+		int num=5;
+		int num2=20;
+		
+		
+		System.out.println("La suma de " +num+ " más " +num2+ " da como resultado: " +(num+num2));
+	}
+
+}
