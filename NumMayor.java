4899babbde4e4331f285327ff1280a5b8f403ae0
L-Guzman <49572675+L-Guzman@users.noreply.github.com>
2019-04-26 05:12:53 -0600

    Add files via upload

---
+++ NumMayor.java
@@ -0,0 +1,20 @@
+
+public class NumMayor {
+	public static void main (String args[]) {
+		
+		int num1=20;
+		int num2=100;
+if (num1>=num2) {
+	if(num1==num2) {
+	System.out.println("Los numeros " +num1+ " y " +num2+  " son iguales");
+	}
+	else {
+		System.out.println("El numero " +num1+ " es mayor que " +num2);
+	}
+}
+else {
+	System.out.println("El numero "+num2+ " es mayor que " +num1);
+}
+	}
+
+}
