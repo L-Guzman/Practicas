88afd642403b5e06e25ecf5f43b73470f3cabb6a
L-Guzman <49572675+L-Guzman@users.noreply.github.com>
2019-04-30 13:27:42 -0600

    Add files via upload

---
+++ HOLA.java
@@ -0,0 +1,12 @@
+
+public class HOLA {
+	public static void main (String args[]) {
+	int num=10;
+	int cont=5;
+	int cambio=num+cont;
+	
+	System.out.println("El número que ingresaste fue: " + num);
+	System.out.println("EL valor por el que cambio es de: " + cont);
+	System.out.println("EL nuevo valor es: " + cambio);
+}
+	}
