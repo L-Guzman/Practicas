775dd5e5b4ce44764a1cc9f769ddb7e1451bb17a
L-Guzman <49572675+L-Guzman@users.noreply.github.com>
2019-05-22 14:58:31 -0600

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,14 @@
+package ciclowhile;
+
+public class dowhile {
+
+	public static void main(String args[]){
+		int cont=0;
+		do {
+	cont++;
+	System.out.println("Valor del contador: " +cont);
+		}
+		while(cont>10);
+	
+	}
+}
