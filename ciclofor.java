819607828edeee1e7ce5f1cb7e5c792401773e11
L-Guzman <49572675+L-Guzman@users.noreply.github.com>
2019-05-22 16:07:06 -0600

    Add files via upload

---
+++ ciclofor.java
@@ -0,0 +1,22 @@
+package ciclofor;
+
+public class ciclofor {
+public static void main(String args[]){
+	/*for(int f=1; f<=10; f++){
+		System.out.println("El valor es: "+f);
+	}*/
+	
+	/*for (int f=1; f<=10; f++){
+		if(f % 2 ==0){
+			System.out.println("Numero par: " +f);
+		}
+	
+	}*/
+	
+	for (int f=1; f<=10; f++){
+		if(f %2 !=0){
+			System.out.println("Numero impar: "+f);
+		}
+	}
+}
+}
