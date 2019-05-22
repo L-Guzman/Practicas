53322bd58a4cf3cafbd3adab79349fe36c6bcd66
L-Guzman <49572675+L-Guzman@users.noreply.github.com>
2019-05-22 14:39:51 -0600

    Add files via upload

---
+++ ciclowhile.java
@@ -0,0 +1,31 @@
+package ciclowhile;
+
+//import javax.swing.JOptionPane;
+
+public class ciclowhile {
+
+	public static void main(String args[]){
+		//Obtiene todo el ciclo
+		
+		int cont=0;
+		/*while(cont<10){
+			cont++;
+			System.out.println( "Valor del contador: " +cont);
+		}*/
+		
+		/*Obtiene numeros pares
+		while(cont<10){
+			cont++;
+			if(cont % 2 ==0){
+				System.out.println("Valor de numeros pares: " +cont);
+			}*/
+		//Obtiene numeros impares
+		while(cont<10){
+			cont ++;
+			if(cont %2 != 0){
+				System.out.println("Valos de numeros impares: "+cont);
+			}
+		}
+		
+		}
+	}
